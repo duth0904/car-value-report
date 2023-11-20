245ed224388f7b886fcfe046303a1d52400ebf86
THANG <thang@THANGs-MacBook-Air.local>
2023-11-20 20:10:43 +0700

    create users and report module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 
 @Module({
-  imports: [UsersModule, ReportsModule],
+  imports: [
+    UsersModule,
+    ReportsModule,
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [User, Report],
+      synchronize: true, // when set true, we not need to migration. TypeORM update all the changes structure of table in database automatically
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
